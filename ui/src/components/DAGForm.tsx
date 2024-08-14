f03fd4ab29e24a68d9828a0380e8312e665df899
Jake C <jakeychap@gmail.com>
2024-08-14 21:45:36 +0100

    creation form can delete parameters + tasks, need to remove old references

--- ui/src/components/DAGForm.tsx
+++ ui/src/components/DAGForm.tsx
@@ -26,6 +26,35 @@ type DAGSpec = {
   parameters?: DagParameterSpec[]; // Global parameters
 };
 
+type DeleteButtonProps = {
+  taskIndex: number;
+  delete: (index: number) => void;
+};
+
+export function DeleteButton(props: DeleteButtonProps) {
+  return (
+    <div
+      class="bg-red-400 rounded-lg p-2 cursor-pointer"
+      onClick={() => {
+        props.delete(props.taskIndex);
+      }}
+    >
+      <svg
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+        fill="white"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M7 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2h4a1 1 0 1 1 0 2h-1.069l-.867 12.142A2 2 0 0 1 17.069 22H6.93a2 2 0 0 1-1.995-1.858L4.07 8H3a1 1 0 0 1 0-2h4zm2 2h6V4H9zM6.074 8l.857 12H17.07l.857-12zM10 10a1 1 0 0 1 1 1v6a1 1 0 1 1-2 0v-6a1 1 0 0 1 1-1m4 0a1 1 0 0 1 1 1v6a1 1 0 1 1-2 0v-6a1 1 0 0 1 1-1"
+          fill="#0D0D0D"
+        />
+      </svg>
+    </div>
+  );
+}
+
 export default function DAGForm() {
   const [parameters, setParameters] = createStore<DagParameterSpec[]>([]);
   const [tasks, setTasks] = createStore<TaskSpec[]>([]);
@@ -53,13 +82,20 @@ export default function DAGForm() {
   };
 
   const addRunAfter = (taskIndex: number) => {
-    console.log(taskIndex, selectedTaskToAdd());
     if (selectedTaskToAdd()) {
       setTasks(taskIndex, "runAfter", [
         ...(tasks[taskIndex].runAfter || []),
         selectedTaskToAdd(),
       ]);
     }
+  };
+
+  const deleteTask = (index: number) => {
+    setTasks((tasks) => [...tasks.slice(0, index), ...tasks.slice(index + 1)]);
+  };
+
+  const deleteParameter = (index: number) => {
+    setParameters((parameters) => [...parameters.slice(0, index), ...parameters.slice(index + 1)]);
   };
 
   const addParameter = () => {
@@ -129,7 +165,10 @@ export default function DAGForm() {
         {(task, i) => (
           <div class="p-4 border rounded-lg bg-gray-700 border-gray-600 space-y-4">
             <div>
-              <label class="block text-lg font-medium">Task Name</label>
+              <label class="text-lg font-medium flex items-center justify-between">
+                Task Name
+                <DeleteButton delete={deleteTask} taskIndex={i()} />
+              </label>
               <input
                 type="text"
                 value={task.name}
@@ -339,7 +378,10 @@ export default function DAGForm() {
           {(param, i) => (
             <div class="mt-4 p-4 border rounded-lg bg-gray-700 border-gray-600 space-y-2">
               <div>
-                <label class="block text-lg font-medium">Parameter Name</label>
+                <label class="text-lg font-medium flex items-center justify-between">
+                  Parameter Name
+                  <DeleteButton  delete={deleteParameter} taskIndex={i()}/>
+                </label>
                 <input
                   type="text"
                   value={param.name}
