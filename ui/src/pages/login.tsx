8b739979dbec7f1f9104f138f901f6a9b2c0d6ca
Jake C <32685500+GreedyKomodoDragon@users.noreply.github.com>
2024-08-26 13:56:45 +0100

    adding image to ui and readme (#18)

--- ui/src/pages/login.tsx
+++ ui/src/pages/login.tsx
@@ -2,12 +2,13 @@ export default function Login() {
   return (
     <main class="w-full h-screen flex flex-col items-center justify-center px-4">
       <div class="max-w-sm w-full text-gray-600 space-y-5">
-        <div class="text-center pb-8">
-          <img src="" width="150" class="mx-auto" />
-        </div>
-        <form class="space-y-5">
+        <img src="/src/assets/logo.svg" width="1500" style={{
+          margin: "0px 0px -100px 0px",
+        }} />
+        <h1 class="text-4xl text-white text-center">Welcome to Kontroler</h1>
+        <form class="">
           <div>
-            <label class="font-medium"> Email </label>
+            <label class="font-medium my-2"> Email </label>
             <input
               type="email"
               required
@@ -15,14 +16,14 @@ export default function Login() {
             />
           </div>
           <div>
-            <label class="font-medium"> Password </label>
+            <label class="font-medium my-2"> Password </label>
             <input
               type="password"
               required
               class="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
             />
           </div>
-          <div class="flex items-center justify-between text-sm">
+          <div class="flex items-center justify-between text-sm my-2">
             <a
               href="javascript:void(0)"
               class="text-center text-indigo-600 hover:text-indigo-500"
