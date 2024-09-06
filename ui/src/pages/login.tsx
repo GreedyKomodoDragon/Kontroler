03049399cce1a3137c02fbd964c8fb5d1c250dc4
Jake C <32685500+GreedyKomodoDragon@users.noreply.github.com>
2024-09-06 22:31:11 +0100

    adding form around inputs to allow enter (#24)

--- ui/src/pages/login.tsx
+++ ui/src/pages/login.tsx
@@ -9,7 +9,8 @@ export default function Login() {
   const [password, setPassword] = createSignal("");
   const [error, setError] = createSignal<string | null>(null);
 
-  const handleLogin = async () => {
+  const handleLogin = async (e: Event) => {
+    e.preventDefault(); // Prevent default form submission behavior (page reload)
     try {
       const successful = await login(username(), password());
       if (successful) {
@@ -17,7 +18,7 @@ export default function Login() {
         return;
       }
 
-      setError("failed to login");
+      setError("Login failed. Please check your username and password.");
     } catch (err) {
       setError("Login failed. Please check your username and password.");
     }
@@ -34,48 +35,41 @@ export default function Login() {
           }}
         />
         <h1 class="text-4xl text-white text-center">Welcome to Kontroler</h1>
-        {error() && <h2>{error()}</h2>}
-        <div>
-          <label class="font-medium my-2"> Username </label>
-          <input
-            type="text"
-            required
-            class="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-            onChange={(e) => setUsername(e.currentTarget.value)}
-          />
-        </div>
-        <div>
-          <label class="font-medium my-2"> Password </label>
-          <input
-            type="password"
-            required
-            class="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-            onChange={(e) => setPassword(e.currentTarget.value)}
-          />
-        </div>
-        <div class="flex items-center justify-between text-sm my-2">
-          <a
-            href="javascript:void(0)"
-            class="text-center text-indigo-600 hover:text-indigo-500"
+        <form onSubmit={handleLogin}>
+          <div>
+            <label class="font-medium my-2"> Username </label>
+            <input
+              type="text"
+              required
+              class="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+              onChange={(e) => setUsername(e.currentTarget.value)}
+            />
+          </div>
+          <div>
+            <label class="font-medium my-2"> Password </label>
+            <input
+              type="password"
+              required
+              class="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+              onChange={(e) => setPassword(e.currentTarget.value)}
+            />
+          </div>
+          <div class="flex items-center justify-between text-sm my-2">
+            <a
+              href="javascript:void(0)"
+              class="text-center text-indigo-600 hover:text-indigo-500"
+            >
+              Forgot password?
+            </a>
+          </div>
+          <button
+            type="submit"
+            class="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
           >
-            Forgot password?
-          </a>
-        </div>
-        <button
-          class="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
-          onClick={handleLogin}
-        >
-          Sign in
-        </button>
-        <p class="text-center">
-          Don't have an account?
-          <a
-            href="javascript:void(0)"
-            class="font-medium text-indigo-600 hover:text-indigo-500 ml-2"
-          >
-            Sign up
-          </a>
-        </p>
+            Sign in
+          </button>
+        </form>
+        {error() && <h2 class="text-red-700" >{error()}</h2>}
       </div>
     </main>
   );
