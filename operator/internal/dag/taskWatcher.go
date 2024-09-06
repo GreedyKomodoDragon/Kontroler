8bf4ada6fa114c9139c237ea6eab4cf91c8fb698
Jake C <32685500+GreedyKomodoDragon@users.noreply.github.com>
2024-09-06 20:27:17 +0100

    removing podwatcher, updating taskwatcher to watch pods, and fixing podTemplate null bug (#22)

--- operator/internal/dag/taskWatcher.go
+++ operator/internal/dag/taskWatcher.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/GreedyKomodoDragon/KubeConductor/operator/api/v1alpha1"
 	"github.com/GreedyKomodoDragon/KubeConductor/operator/internal/db"
-	batchv1 "k8s.io/api/batch/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/informers"
@@ -22,7 +22,8 @@ const (
 	ALL_NAMESPACES string = ""
 )
 
-// Purpose of TaskWatcher is to listen for pods within a job to finish and record results/trigger the next pods
+// Purpose of TaskWatcher is to listen for pods to finish and record results/trigger the next pods
+// Will also allocate new pods
 type TaskWatcher interface {
 	StartWatching()
 }
@@ -44,12 +45,13 @@ func NewTaskWatcher(clientSet *kubernetes.Clientset, taskAllocator TaskAllocator
 	}).AsSelector().String()
 
 	// Create a factory that watches all namespaces
-	factory := informers.NewSharedInformerFactoryWithOptions(clientSet, 0, informers.WithTweakListOptions(func(options *metav1.ListOptions) {
+	// Set resync period to 30 seconds
+	factory := informers.NewSharedInformerFactoryWithOptions(clientSet, 30*time.Second, informers.WithTweakListOptions(func(options *metav1.ListOptions) {
 		options.LabelSelector = labelSelector
 	}))
 
-	// Create an informer that watches jobs with the specified label selector
-	informer := factory.Batch().V1().Jobs().Informer()
+	// Create an informer that watches pods with the specified label selector
+	informer := factory.Core().V1().Pods().Informer()
 
 	watcher := &taskWatcher{
 		dbManager:     dbManager,
@@ -62,8 +64,8 @@ func NewTaskWatcher(clientSet *kubernetes.Clientset, taskAllocator TaskAllocator
 
 	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc:    watcher.handleAdd,
-		UpdateFunc: func(oldObj, newObj interface{}) { watcher.handleUpdate(newObj) },
-		DeleteFunc: watcher.handleJobDelete,
+		UpdateFunc: func(oldObj, newObj interface{}) { watcher.handleUpdate(oldObj, newObj) },
+		DeleteFunc: watcher.handlePodDelete,
 	})
 
 	return watcher, nil
@@ -76,42 +78,65 @@ func (t *taskWatcher) StartWatching() {
 }
 
 func (t *taskWatcher) handleAdd(obj interface{}) {
-	job, ok := obj.(*batchv1.Job)
+	eventTime := time.Now()
+
+	pod, ok := obj.(*v1.Pod)
 	if !ok {
-		log.Log.Error(fmt.Errorf("invalid object"), "failed to parse job object")
+		log.Log.Error(fmt.Errorf("invalid object"), "failed to parse pod object")
 		return
 	}
 
-	// Filter out jobs created before the watcher started
-	if job.CreationTimestamp.Time.Before(t.startTime) {
+	// Filter out pods created before the watcher started
+	if pod.CreationTimestamp.Time.Before(t.startTime) {
 		return
 	}
 
-	t.handleOutcome(job, "add")
+	t.handleOutcome(pod, "add")
+
+	if err := t.writeStatusToDB(pod, eventTime); err != nil {
+		log.Log.Error(err, "failed to writeStatusToDB")
+	}
 }
 
-func (t *taskWatcher) handleUpdate(obj interface{}) {
-	job, ok := obj.(*batchv1.Job)
+func (t *taskWatcher) handleUpdate(old, obj interface{}) {
+	eventTime := time.Now()
+	oldPod, ok := old.(*v1.Pod)
 	if !ok {
-		log.Log.Error(fmt.Errorf("invalid object"), "failed to parse job object")
+		log.Log.Error(fmt.Errorf("invalid object"), "failed to parse old pod object in handleUpdate")
 		return
 	}
 
-	// Filter out jobs created before the watcher started
-	if job.CreationTimestamp.Time.Before(t.startTime) {
+	pod, ok := obj.(*v1.Pod)
+	if !ok {
+		log.Log.Error(fmt.Errorf("invalid object"), "failed to parse pod object")
 		return
 	}
 
-	t.handleOutcome(job, "update")
+	if oldPod.Status.Phase == pod.Status.Phase {
+		return
+	}
+
+	// Filter out pods created before the watcher started
+	if pod.CreationTimestamp.Time.Before(t.startTime) {
+		return
+	}
+
+	t.handleOutcome(pod, "update")
+
+	log.Log.Info("pod event", "podUID", pod.UID, "name", pod.Name, "event", "update", "status", pod.Status.Phase)
+
+	if err := t.writeStatusToDB(pod, eventTime); err != nil {
+		log.Log.Error(err, "failed to writeStatusToDB")
+	}
 }
 
-func (t *taskWatcher) handleOutcome(job *batchv1.Job, event string) {
+func (t *taskWatcher) handleOutcome(pod *v1.Pod, event string) {
 	ctx := context.Background()
-	log.Log.Info("job event", "jobUid", job.UID, "event", event)
+	log.Log.Info("pod event", "podUID", pod.UID, "name", pod.Name, "event", event)
 
-	taskRunIdStr, ok := job.Annotations["kubeconductor/task-rid"]
+	taskRunIdStr, ok := pod.Annotations["kubeconductor/task-rid"]
 	if !ok {
-		log.Log.Error(fmt.Errorf("missing annotation"), "annotation", "kubeconductor/task-rid", "job", job.Name)
+		log.Log.Error(fmt.Errorf("missing annotation"), "annotation", "kubeconductor/task-rid", "pod", pod.Name)
 		return
 	}
 
@@ -121,30 +146,62 @@ func (t *taskWatcher) handleOutcome(job *batchv1.Job, event string) {
 		return
 	}
 
-	if job.Status.Succeeded > 0 {
-		t.handleSuccessfulJob(ctx, job, taskRunId)
-	} else if job.Status.Failed > 0 {
-		t.handleFailedJob(ctx, job, taskRunId)
-	}
-}
-
-func (t *taskWatcher) handleJobDelete(obj interface{}) {
-	job, ok := obj.(*batchv1.Job)
-	if !ok {
-		log.Log.Error(fmt.Errorf("invalid object"), "failed to parse job object")
+	// Ensure container status exists
+	if len(pod.Status.ContainerStatuses) == 0 {
+		log.Log.Info("no container status available", "podUID", pod.UID, "name", pod.Name, "event", event)
 		return
 	}
 
-	log.Log.Info("job was deleted", "jobUid", job.UID)
+	containerStatus := pod.Status.ContainerStatuses[0]
+	state := containerStatus.State
+
+	// Check if container is still running or waiting
+	if state.Running != nil {
+		log.Log.Info("pod is still running", "podUID", pod.UID, "name", pod.Name, "event", event, "startedAt", state.Running.StartedAt)
+		return
+	}
+
+	if state.Waiting != nil {
+		log.Log.Info("pod is waiting", "podUID", pod.UID, "name", pod.Name, "event", event, "reason", state.Waiting.Reason, "message", state.Waiting.Message)
+		return
+	}
+
+	// Check if the container has terminated
+	if state.Terminated == nil {
+		return
+	}
+
+	terminatedState := state.Terminated
+	log.Log.Info("pod terminated", "podUID", pod.UID, "name", pod.Name, "event", event, "exitCode", terminatedState.ExitCode, "reason", terminatedState.Reason, "message", terminatedState.Message, "finishedAt", terminatedState.FinishedAt)
+
+	// Log specific success/failure
+	if terminatedState.ExitCode == 0 {
+		log.Log.Info("pod completed successfully", "podUID", pod.UID, "name", pod.Name, "event", event, "exitCode", terminatedState.ExitCode)
+		t.handleSuccessfulTaskRun(ctx, pod, taskRunId)
+		return
+	}
+
+	log.Log.Info("pod failed", "podUID", pod.UID, "name", pod.Name, "exitCode", terminatedState.ExitCode, "reason", terminatedState.Reason)
+	t.handleFailedTaskRun(ctx, pod, taskRunId)
 }
 
-func (t *taskWatcher) handleSuccessfulJob(ctx context.Context, job *batchv1.Job, taskRunId int) {
-	log.Log.Info("task succeeded", "jobUid", job.UID, "taskRunId", taskRunId)
-
-	// Get the run ID from the job
-	dagRunIdStr, ok := job.Annotations["kubeconductor/dagRun-id"]
+func (t *taskWatcher) handlePodDelete(obj interface{}) {
+	pod, ok := obj.(*v1.Pod)
 	if !ok {
-		log.Log.Error(fmt.Errorf("missing annotation"), "annotation", "kubeconductor/dagRun-id", "job", job.Name)
+		log.Log.Error(fmt.Errorf("invalid object"), "failed to parse pod object")
+		return
+	}
+
+	log.Log.Info("pod was deleted", "podUid", pod.UID)
+}
+
+func (t *taskWatcher) handleSuccessfulTaskRun(ctx context.Context, pod *v1.Pod, taskRunId int) {
+	log.Log.Info("task succeeded", "podUID", pod.UID, "name", pod.Name, "taskRunId", taskRunId)
+
+	// Get the run ID from the Pod
+	dagRunIdStr, ok := pod.Annotations["kubeconductor/dagRun-id"]
+	if !ok {
+		log.Log.Error(fmt.Errorf("missing annotation"), "annotation", "kubeconductor/dagRun-id", "pod", pod.Name)
 		return
 	}
 
@@ -158,7 +215,7 @@ func (t *taskWatcher) handleSuccessfulJob(ctx context.Context, job *batchv1.Job,
 	t.lock.Lock()
 	tasks, err := t.dbManager.MarkSuccessAndGetNextTasks(ctx, taskRunId)
 	if err != nil {
-		log.Log.Error(err, "failed to mark outcome and get next task", "jobUid", job.UID, "event", "add/update")
+		log.Log.Error(err, "failed to mark outcome and get next task", "podUID", pod.UID, "name", pod.Name, "event", "add/update")
 		t.lock.Unlock()
 		return
 	}
@@ -166,6 +223,7 @@ func (t *taskWatcher) handleSuccessfulJob(ctx context.Context, job *batchv1.Job,
 
 	log.Log.Info("number of tasks", "tasks", len(tasks))
 
+	// TODO: Using a channel + Goroutines Workers for scaling out pods quicker
 	for _, task := range tasks {
 		taskRunId, err := t.dbManager.MarkTaskAsStarted(ctx, runId, task.Id)
 		if err != nil {
@@ -173,118 +231,120 @@ func (t *taskWatcher) handleSuccessfulJob(ctx context.Context, job *batchv1.Job,
 			continue
 		}
 
-		job, err := t.taskAllocator.AllocateTask(ctx, task, runId, taskRunId, job.Namespace)
+		newPod, err := t.taskAllocator.AllocateTask(ctx, task, runId, taskRunId, pod.Namespace)
 		if err != nil {
 			log.Log.Error(err, "failed to allocate task", "task.Id", task.Id, "task.Name", task.Name)
 			continue
 		}
 
-		log.Log.Info("allocated task", "jobId", job, "task.Id", task.Id, "task.Name", task.Name)
+		log.Log.Info("allocated task", "newPodUID", newPod, "task.Id", task.Id, "task.Name", task.Name)
 	}
 
-	backgroundDeletion := metav1.DeletePropagationBackground
-	if err := t.clientSet.BatchV1().Jobs(job.Namespace).Delete(ctx, job.Name, metav1.DeleteOptions{
-		PropagationPolicy: &backgroundDeletion,
-	}); err != nil {
-		log.Log.Error(err, "failed to delete job in failed", "jobId", job)
+	if err := t.deletePod(ctx, pod); err != nil {
+		log.Log.Error(err, "failed to delete pod in successful", "podUId", pod.UID)
 	}
 }
 
-func (t *taskWatcher) handleFailedJob(ctx context.Context, job *batchv1.Job, taskRunId int) {
-	log.Log.Info("task failed", "jobUid", job.UID, "taskRunId", taskRunId)
+func (t *taskWatcher) handleFailedTaskRun(ctx context.Context, pod *v1.Pod, taskRunId int) {
+	log.Log.Info("task failed", "podUID", pod.UID, "name", pod.Name, "taskRunId", taskRunId, "exitcode", pod.Status.ContainerStatuses[0].State.Terminated.ExitCode)
 
-	// Extract failure details
-	pods, err := t.clientSet.CoreV1().Pods(job.Namespace).List(ctx, metav1.ListOptions{
-		LabelSelector: fmt.Sprintf("job-name=%s", job.Name),
-	})
-
-	if err != nil {
-		log.Log.Error(err, "failed to list pods for job", "job", job.Name)
+	dagRunStr, ok := pod.Annotations["kubeconductor/dagRun-id"]
+	if !ok {
+		log.Log.Error(fmt.Errorf("find to find annotation"), "found pod missing kubeconductor/dagRun-id", "pod", pod.Name)
 		return
 	}
 
-	// log.Log.Info("number of pods in job", "jobUid", job.UID, "count", len(pods.Items))
-
-	// Get Exitable codes to restart with and backoff limit
-	for _, pod := range pods.Items {
-
-		dagRunStr, ok := job.Annotations["kubeconductor/dagRun-id"]
-		if !ok {
-			log.Log.Error(err, "found pod missing kubeconductor/dagRun-id", "job", job.Name)
-			continue
-		}
-
-		if len(pod.Status.ContainerStatuses) == 0 {
-			log.Log.Error(err, "does not seem to be any containers?", "job", job.Name)
-			continue
-		}
-
-		if pod.Status.ContainerStatuses[0].State.Terminated == nil {
-			log.Log.Error(err, "missing status informationt", "job", job.Name)
-			continue
-		}
-
-		backgroundDeletion := metav1.DeletePropagationBackground
-		if err := t.clientSet.BatchV1().Jobs(job.Namespace).Delete(ctx, job.Name, metav1.DeleteOptions{
-			PropagationPolicy: &backgroundDeletion,
-		}); err != nil {
-			log.Log.Error(err, "failed to delete job in failed", "jobId", job)
-			continue
-		}
-
-		ok, err := t.dbManager.ShouldRerun(ctx, taskRunId, pod.Status.ContainerStatuses[0].State.Terminated.ExitCode)
-		if err != nil {
-			log.Log.Error(err, "failed to determine if pod should be re-ran", "job", job.Name)
-			continue
-		}
-
-		if !ok {
-			log.Log.Info("job has reached it max backoffLimit or exit code not recoverable", "jobUid", job.UID, "exitCode", pod.Status.ContainerStatuses[0].State.Terminated.ExitCode)
-
-			if err := t.dbManager.MarkTaskAsFailed(ctx, taskRunId); err != nil {
-				log.Log.Error(err, "failed to mark task as failed", "jobUid", job.UID, "event", "add/update")
-			}
-
-			continue
-		}
-
-		dagRunId, err := strconv.Atoi(dagRunStr)
-		if err != nil {
-			log.Log.Error(err, "failed to parse dagRunStr", "dagRunStr", dagRunStr)
-			continue
-		}
-
-		container := pod.Spec.Containers[0]
-		// Re-use envs instead of going to database again
-		taskId, err := t.taskAllocator.AllocateTaskWithEnv(ctx,
-			db.Task{
-				Name:    container.Name,
-				Args:    container.Args,
-				Command: container.Command,
-				Image:   container.Image,
-				PodTemplate: &v1alpha1.PodTemplateSpec{
-					Volumes:                      pod.Spec.Volumes,
-					VolumeMounts:                 container.VolumeMounts,
-					ImagePullSecrets:             pod.Spec.ImagePullSecrets,
-					SecurityContext:              pod.Spec.SecurityContext,
-					NodeSelector:                 pod.Spec.NodeSelector,
-					Tolerations:                  pod.Spec.Tolerations,
-					Affinity:                     pod.Spec.Affinity,
-					ServiceAccountName:           pod.Spec.ServiceAccountName,
-					AutomountServiceAccountToken: pod.Spec.AutomountServiceAccountToken,
-				},
-			}, dagRunId, taskRunId, pod.Namespace, container.Env, &container.Resources)
-
-		if err != nil {
-			log.Log.Error(err, "failed to allocate new pod")
-			continue
-		}
-
-		if err := t.dbManager.IncrementAttempts(ctx, taskRunId); err != nil {
-			log.Log.Error(err, "failed to increment attempts", "taskRunId", taskRunId)
-		}
-
-		log.Log.Info("new task allocated allocated with env", "taskId", taskId)
-
+	if err := t.deletePod(ctx, pod); err != nil {
+		log.Log.Error(err, "failed to delete pod in failed", "podUId", pod.UID)
+		return
 	}
+
+	ok, err := t.dbManager.ShouldRerun(ctx, taskRunId, pod.Status.ContainerStatuses[0].State.Terminated.ExitCode)
+	if err != nil {
+		log.Log.Error(err, "failed to determine if pod should be re-ran", "pod", pod.Name)
+		return
+	}
+
+	if !ok {
+		log.Log.Info("pod has reached it max backoffLimit or exit code not recoverable", "podUID", pod.UID, "name", pod.Name, "exitCode", pod.Status.ContainerStatuses[0].State.Terminated.ExitCode)
+
+		if err := t.dbManager.MarkTaskAsFailed(ctx, taskRunId); err != nil {
+			log.Log.Error(err, "failed to mark task as failed", "podUID", pod.UID, "name", pod.Name, "event", "add/update")
+		}
+
+		return
+	}
+
+	dagRunId, err := strconv.Atoi(dagRunStr)
+	if err != nil {
+		log.Log.Error(err, "failed to parse dagRunStr", "dagRunStr", dagRunStr)
+		return
+	}
+
+	container := pod.Spec.Containers[0]
+	// Re-use envs instead of going to database again
+	taskId, err := t.taskAllocator.AllocateTaskWithEnv(ctx,
+		db.Task{
+			Name:    container.Name,
+			Args:    container.Args,
+			Command: container.Command,
+			Image:   container.Image,
+			PodTemplate: &v1alpha1.PodTemplateSpec{
+				Volumes:                      pod.Spec.Volumes,
+				VolumeMounts:                 container.VolumeMounts,
+				ImagePullSecrets:             pod.Spec.ImagePullSecrets,
+				SecurityContext:              pod.Spec.SecurityContext,
+				NodeSelector:                 pod.Spec.NodeSelector,
+				Tolerations:                  pod.Spec.Tolerations,
+				Affinity:                     pod.Spec.Affinity,
+				ServiceAccountName:           pod.Spec.ServiceAccountName,
+				AutomountServiceAccountToken: pod.Spec.AutomountServiceAccountToken,
+			},
+		}, dagRunId, taskRunId, pod.Namespace, container.Env, &container.Resources)
+
+	if err != nil {
+		log.Log.Error(err, "failed to allocate new pod")
+		return
+	}
+
+	if err := t.dbManager.IncrementAttempts(ctx, taskRunId); err != nil {
+		log.Log.Error(err, "failed to increment attempts", "taskRunId", taskRunId)
+	}
+
+	log.Log.Info("new task allocated allocated with env", "taskId", taskId)
+
+}
+
+func (t *taskWatcher) writeStatusToDB(pod *v1.Pod, stamp time.Time) error {
+	taskRunIDStr, ok := pod.Annotations["kubeconductor/task-rid"]
+	if !ok {
+		return fmt.Errorf("missing annotation kubeconductor/task-rid")
+	}
+
+	taskRunId, err := strconv.Atoi(taskRunIDStr)
+	if err != nil {
+		return fmt.Errorf("failed to convert task run string: %s", taskRunIDStr)
+	}
+
+	var exitCode *int32 = nil
+	if len(pod.Status.ContainerStatuses) > 0 && pod.Status.ContainerStatuses[0].State.Terminated != nil {
+		exitCode = &pod.Status.ContainerStatuses[0].State.Terminated.ExitCode
+	}
+
+	if err := t.dbManager.MarkPodStatus(context.Background(), pod.UID, pod.Name, taskRunId, pod.Status.Phase, stamp, exitCode); err != nil {
+		return fmt.Errorf("failed to mark pods status: %v", pod.Status.Phase)
+	}
+
+	return nil
+}
+
+func (t *taskWatcher) deletePod(ctx context.Context, pod *v1.Pod) error {
+	backgroundDeletion := metav1.DeletePropagationBackground
+	if err := t.clientSet.CoreV1().Pods(pod.Namespace).Delete(ctx, pod.Name, metav1.DeleteOptions{
+		PropagationPolicy: &backgroundDeletion,
+	}); err != nil {
+		return err
+	}
+
+	return nil
 }
