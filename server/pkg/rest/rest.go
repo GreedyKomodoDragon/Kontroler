9a9af6d14f717ed235b891f72071320da6bb6245
Jake C <jakeychap@gmail.com>
2024-09-29 12:19:52 +0100

    adding mtls to server + adding reverse proxy to nginx config in helm for ui

--- server/pkg/rest/rest.go
+++ server/pkg/rest/rest.go
@@ -1,8 +1,12 @@
 package rest
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
 	"kontroler-server/pkg/auth"
 	"kontroler-server/pkg/db"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -33,4 +37,36 @@ func NewFiberHttpServer(dbManager db.DbManager, kClient dynamic.Interface, authM
 	addV1(app, dbManager, kClient, authManager)
 
 	return app
+}
+
+func CreateTLSConfig() (*tls.Config, error) {
+	cert, err := tls.LoadX509KeyPair("/tls.crt", "/tls.key")
+	if err != nil {
+		return nil, fmt.Errorf("failed to load server certificate: %v", err)
+	}
+
+	caCert, err := os.ReadFile("/ca.crt")
+	if err != nil {
+		return nil, fmt.Errorf("failed to load CA certificate: %v", err)
+	}
+
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Create base TLS configuration
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ClientCAs:    caCertPool,
+	}
+
+	if os.Getenv("INSECURE") == "true" {
+		// In insecure mode
+		tlsConfig.ClientAuth = tls.RequireAnyClientCert
+		tlsConfig.InsecureSkipVerify = true
+		return tlsConfig, nil
+	}
+
+	// In secure mode (mTLS), client certificates are required
+	tlsConfig.ClientAuth = tls.RequireAndVerifyClientCert
+	return tlsConfig, nil
 }
