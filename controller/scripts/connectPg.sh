20b618b5bee07af52ef498c987e8eb3865b78bce
Jake C <jakeychap@gmail.com>
2024-10-02 22:06:08 +0100

    server is handle tls connections to postgresql

--- controller/scripts/connectPg.sh
+++ controller/scripts/connectPg.sh
@@ -1,3 +1,6 @@
 export POSTGRES_PASSWORD=$(kubectl get secret --namespace default my-release-postgresql -o jsonpath="{.data.postgres-password}" | base64 -d)
-kubectl run my-release-postgresql-client --rm --tty -i --restart='Never' --namespace default --image docker.io/bitnami/postgresql:16.4.0-debian-12-r7 --env="PGPASSWORD=$POSTGRES_PASSWORD" \
-      --command -- psql --host my-release-postgresql -U postgres -d postgres -p 5432
+export SSL_MODE=require  # Change this to 'disable', 'require', 'verify-ca', or 'verify-full' as needed
+
+kubectl run my-release-postgresql-client --rm --tty -i --restart='Never' --namespace default --image docker.io/bitnami/postgresql:16.4.0-debian-12-r7 \
+      --env="PGPASSWORD=$POSTGRES_PASSWORD" --env="PGSSLMODE=$SSL_MODE" \
+      --command -- psql --host my-release-postgresql -U postgres -d postgres -p 5432 --set=sslmode=$SSL_MODE
