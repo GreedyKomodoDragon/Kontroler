17daa8af9a01fb3bb18f3c4faaf2f26e53d672e3
Jake C <jakeychap@gmail.com>
2024-12-31 23:53:48 +0000

    tidying up backing service scripts into a single skaffold config for minikube

--- controller/install-minio.sh
+++
@@ -1,1 +0,0 @@
-helm install minio oci://registry-1.docker.io/bitnamicharts/minio -f minio.yaml
--- scripts/install-postgresql.sh
+++
@@ -1,8 +0,0 @@
-helm upgrade -i my-release oci://registry-1.docker.io/bitnamicharts/postgresql \
-    --set tls.enabled=true \
-    --set tls.certificatesSecret=postgresql-tls \
-    --set tls.certFilename=postgresql.crt \
-    --set tls.certKeyFilename=postgresql.key \
-    -f postgres.yaml
-
-# --set tls.certCAFilename=ca.crt \
