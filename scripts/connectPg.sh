ab6a2efd3f8cd500b3d6fbbe20fa188c530557a9
Jake C <jakeychap@gmail.com>
2024-12-31 23:53:48 +0000

    tidying up backing service scripts into a single skaffold config for minikube

--- scripts/connectPg.sh
+++ scripts/connectPg.sh
@@ -1,4 +1,4 @@
-export POSTGRES_PASSWORD=$(kubectl get secret --namespace default postgres-kontroler-password -o jsonpath="{.data.password}" | base64 -d)
+export POSTGRES_PASSWORD=$(kubectl get secret --namespace default postgres-postgresql -o jsonpath="{.data.postgres-password}" | base64 -d)
 export SSL_MODE=require  # Change this to 'disable', 'require', 'verify-ca', or 'verify-full' as needed
 
 kubectl run my-release-postgresql-client --rm --tty -i --restart='Never' --namespace default --image docker.io/bitnami/postgresql:16.4.0-debian-12-r7 \
