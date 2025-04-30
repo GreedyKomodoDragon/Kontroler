3a06989b22c2886e5dcc43888dca91f83713e762
Jake C <jakeychap@gmail.com>
2025-04-30 12:31:02 +0100

    fix: missing comma in nginx config

--- ui/entrypoint.sh
+++ ui/entrypoint.sh
@@ -3,7 +3,7 @@
 # Generate the env-config.js file with the API_URL
 cat <<EOF > /usr/share/nginx/html/env-config.js
 window.__ENV__ = {
-  API_URL: "${API_URL:-http://localhost:8080}"
+  API_URL: "${API_URL:-http://localhost:8080}",
   WS_URL: "${WS_URL:-ws://localhost:8080}"
 };
 EOF
