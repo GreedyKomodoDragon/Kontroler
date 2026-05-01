d05f840b2155e915e18c72f4c47e2b36f66ecefe
Jake C <jakeychap@gmail.com>
2026-05-01 21:37:02 +0100

    Clean up repo: remove secret files and helper scripts
    - Remove xata.yaml (contains JWT secret)
    - Remove kubeconfig-secret.yaml (contains kubeconfig secret)
    - Remove scripts/ directory (helper scripts not part of core code)
    - Update .gitignore to exclude:
      - xata.yaml
      - kubeconfig-secret.yaml
      - scripts/
    
    These files were mistakenly added during monorepo merge and contain
    sensitive data or are non-essential helper scripts.

--- scripts/convertCerts.sh
+++
@@ -1,5 +0,0 @@
-openssl pkcs12 -export \
-    -in certs/server.crt \
-    -inkey certs/server.key \
-    -certfile certs/ca.crt \
-    -out client-cert.p12
--- scripts/minikube/wipecache.sh
+++
@@ -1,3 +0,0 @@
-for image in $(minikube cache list); do
-    minikube cache delete $image
-done
