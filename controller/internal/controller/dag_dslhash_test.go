12bb48b43e7abf20c7106699e98949079b4f1751
Jake C <jakeychap@gmail.com>
2026-05-07 20:43:27 +0100

    tests: add focused DSL-hash tests (write and skip behavior)

---
+++ controller/internal/controller/dag_dslhash_test.go
@@ -0,0 +1,102 @@
+package controller
+
+import (
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	kontrolerv1alpha1 "kontroler-controller/api/v1alpha1"
+	"kontroler-controller/internal/db"
+
+	cron "github.com/robfig/cron/v3"
+)
+
+var _ = Describe("DAG DSL hash behavior", func() {
+	ctx := context.Background()
+
+	It("writes a kontoler/dsl-hash annotation when processing DSL", func() {
+		name := "dsl-hash-write"
+		nn := types.NamespacedName{Name: name, Namespace: "default"}
+
+		dsl := `schedule "0 */6 * * *"
+
+parameters {
+  environment {
+    default "production"
+  }
+}`
+
+		dag := &kontrolerv1alpha1.DAG{
+			ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: "default"},
+			Spec:       kontrolerv1alpha1.DAGSpec{DSL: dsl},
+		}
+
+		Expect(k8sClient.Create(ctx, dag)).To(Succeed())
+
+		// Setup DB
+		parser := cron.NewParser(cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow)
+		config := &db.SQLiteConfig{DBPath: ":memory:", JournalMode: "WAL", Synchronous: "NORMAL", CacheSize: -2000, TempStore: "MEMORY"}
+		dbManager, _, err := db.NewSqliteManager(context.TODO(), &parser, config)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(dbManager.InitaliseDatabase(context.TODO())).To(Succeed())
+
+		reconciler := &DAGReconciler{Client: k8sClient, Scheme: k8sClient.Scheme(), DbManager: dbManager}
+		_, err = reconciler.Reconcile(ctx, ctrl.Request{NamespacedName: nn})
+		Expect(err).NotTo(HaveOccurred())
+
+		reconciled := &kontrolerv1alpha1.DAG{}
+		Expect(k8sClient.Get(ctx, nn, reconciled)).To(Succeed())
+
+		hash := sha256.Sum256([]byte(reconciled.Spec.DSL))
+		hs := hex.EncodeToString(hash[:])
+		Expect(reconciled.Annotations).NotTo(BeNil())
+		Expect(reconciled.Annotations["kontroler/dsl-hash"]).To(Equal(hs))
+
+		Expect(k8sClient.Delete(ctx, reconciled)).To(Succeed())
+	})
+
+	It("skips processing when kontoler/dsl-hash already present", func() {
+		name := "dsl-hash-skip"
+		nn := types.NamespacedName{Name: name, Namespace: "default"}
+
+		dsl := `schedule "0 */6 * * *"
+
+parameters {
+  environment {
+    default "production"
+  }
+}`
+		hash := sha256.Sum256([]byte(dsl))
+		hs := hex.EncodeToString(hash[:])
+
+		dag := &kontrolerv1alpha1.DAG{
+			ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: "default", Annotations: map[string]string{"kontroler/dsl-hash": hs}},
+			Spec:       kontrolerv1alpha1.DAGSpec{DSL: dsl},
+		}
+
+		Expect(k8sClient.Create(ctx, dag)).To(Succeed())
+
+		parser := cron.NewParser(cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow)
+		config := &db.SQLiteConfig{DBPath: ":memory:", JournalMode: "WAL", Synchronous: "NORMAL", CacheSize: -2000, TempStore: "MEMORY"}
+		dbManager, _, err := db.NewSqliteManager(context.TODO(), &parser, config)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(dbManager.InitaliseDatabase(context.TODO())).To(Succeed())
+
+		reconciler := &DAGReconciler{Client: k8sClient, Scheme: k8sClient.Scheme(), DbManager: dbManager}
+		_, err = reconciler.Reconcile(ctx, ctrl.Request{NamespacedName: nn})
+		Expect(err).NotTo(HaveOccurred())
+
+		observed := &kontrolerv1alpha1.DAG{}
+		Expect(k8sClient.Get(ctx, nn, observed)).To(Succeed())
+		// schedule should remain empty as processing was skipped
+		Expect(observed.Spec.Schedule).To(Equal(""))
+
+		Expect(k8sClient.Delete(ctx, observed)).To(Succeed())
+	})
+})
